aeee97e49a5d26f947ee3801132e7752904dd9bb
brionesl <143744654+LeonelAkio@users.noreply.github.com>
2024-01-10 19:11:51 +0000

    yes

--- exercice_poo2.py
+++ exercice_poo2.py
@@ -1,7 +1,12 @@
-#crée par leonel akio briones (avec un peut d'aide de mon père)
+#crée par leonel akio briones
 
 import random
-#définis la classe npc
+from colorama import init, Fore
+
+# Initialiser colorama pour avoir un output lisible
+init(autoreset=True)
+
+#crée la classe npc
 class NPC:
     def __init__(self, nom, race, espèce, profession):
         self.nom = nom
@@ -9,6 +14,7 @@ class NPC:
         self.espèce = espèce
         self.profession = profession
 
+        # Initialisation des caractéristiques avec des dés
         self.force = self._lancer_des(4, 6)
         self.agilité = self._lancer_des(4, 6)
         self.constitution = self._lancer_des(4, 6)
@@ -16,14 +22,17 @@ class NPC:
         self.sagesse = self._lancer_des(4, 6)
         self.charisme = self._lancer_des(4, 6)
 
-        self.classe_armure = self._lancer_des(1, 12)
-        self.points_de_vie = self._lancer_des(1, 20)
+        # Initialisation de la classe d'armure et des points de vie
+        self.classe_armure = self._lancer_des(2, 12)
+        self.points_de_vie = self._lancer_des(2, 20)
 
     def _lancer_des(self, nb_des, faces):
+        # Simulation d'un lancer de dés classique
         return sum(sorted([random.randint(1, faces) for _ in range(nb_des)])[1:])
-#définis la fonction qui montre les caractéristiques
+
     def afficher_caracteristiques(self):
-        print(f"Nom: {self.nom}")
+        # Affichage des caractéristiques du NPC
+        print(f"{Fore.GREEN}Nom: {self.nom}")
         print(f"Race: {self.race}")
         print(f"Espèce: {self.espèce}")
         print(f"Profession: {self.profession}")
@@ -35,28 +44,29 @@ class NPC:
         print(f"Charisme: {self.charisme}")
         print(f"Classe d'armure: {self.classe_armure}")
         print(f"Points de vie: {self.points_de_vie}")
-#définis la classe Kobold
+#classe kobold
 class Kobold(NPC):
+#définit la fonction d'attaque
     def attaquer(self, cible):
-        print(f"{self.nom} attaque {cible.nom}!")
-
+        print(f"{Fore.RED}{self.nom} attaque {cible.nom}!")
+#recevoir le dommage
     def subir_dommage(self, quantite):
-        print(f"{self.nom} subit {quantite} points de dommage!")
-#définis la classe Héros
+        print(f"{Fore.RED}{self.nom} subit {quantite} points de dommage!")
+#classe héro
 class Heros(NPC):
+#définit la fonction
     def attaquer(self, cible):
-        print(f"{self.nom} attaque {cible.nom}!")
-
+        print(f"{Fore.BLUE}{self.nom} attaque {cible.nom}!")
+#recevoir le dommage
     def subir_dommage(self, quantite):
-        print(f"{self.nom} subit {quantite} points de dommage!")
+        print(f"{Fore.BLUE}{self.nom} subit {quantite} points de dommage!")
 
 # Exemple d'utilisation
 kobold = Kobold(nom="Kobold1", race="Kobold", espèce="Reptile", profession="Guerrier")
 heros = Heros(nom="Héros1", race="Humain", espèce="Humanoid", profession="Paladin")
-#utilise les fonctions créé
+
 kobold.afficher_caracteristiques()
 heros.afficher_caracteristiques()
-#le kobold attaque
+
 kobold.attaquer(heros)
-#le héro subis les dommages
 heros.subir_dommage(4)
